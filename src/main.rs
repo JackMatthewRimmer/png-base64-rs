e59bafaca50c533063b4084c03856a0cbad4ce99
JackMatthewRimmer <rimbojnr@gmail.com>
2024-06-01 11:17:52 +0100

    Add chunk reading

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::fs;
 use png_base64_rs::PNG;
+use std::fs;
 
 fn main() {
     let file_contents = fs::read("src/test-image.png").unwrap();
-    let _png: PNG = PNG::from_bytes(&file_contents);    
+    let _png: PNG = PNG::from_bytes(&file_contents);
 }
--- src/png.rs
+++ src/png.rs
@@ -1,3 +1,4 @@
+use std::slice;
 
 pub struct PNG {}
 
@@ -6,6 +7,8 @@ impl PNG {
 
     pub fn from_bytes(bytes: &[u8]) -> Self {
         let chunk_bytes: &[u8] = Self::read_and_check_file_signature(bytes);
+        let (chunk, rem) = Self::read_chunk(chunk_bytes);
+        dbg!(chunk);
         PNG {}
     }
 
@@ -14,18 +17,56 @@ impl PNG {
         assert_eq!(signature, Self::SIGNATURE);
         &bytes[8..]
     }
-}
 
+    // Function returns a chunk and the remaining bytes
+    fn read_chunk<'a>(bytes: &'a [u8]) -> (Chunk<'a>, &'a [u8]) {
+        let size_bytes: [u8; 4] = bytes
+            .get(0..4)
+            .and_then(|size_bytes| size_bytes.try_into().ok())
+            .expect("Failed to read chunk size");
+
+        let size_32: u32 = u32::from_be_bytes(size_bytes);
+        let end_of_data_index: usize = (size_32 + 8) as usize;
+
+        let chunk_type: &[u8; 4] = bytes
+            .get(4..8)
+            .and_then(|size_bytes| size_bytes.try_into().ok())
+            .expect("Failed to read chunk type");
+
+        let chunk_data: &[u8] = bytes
+            .get(8..end_of_data_index)
+            .expect("Failed to read to chunk data");
+
+        let crc: &[u8; 4] = bytes
+            .get(end_of_data_index..end_of_data_index + 4)
+            .and_then(|crc_bytes| crc_bytes.try_into().ok())
+            .expect("Failed to read crc");
+
+        let chunk: Chunk = Chunk {
+            size: size_32,
+            chunk_type,
+            chunk_data,
+            crc,
+        };
+
+        let remaining_bytes = bytes
+            .get(end_of_data_index + 4..)
+            .expect("Failed to get remaining bytes");
+
+        (chunk, remaining_bytes)
+    }
+}
 
 /// Idea here is read in a raw chunk and then parse it based on the chunk_type
+#[derive(Debug)]
 pub struct Chunk<'a> {
-    size: &'a [u8; 4],
+    size: u32,
     chunk_type: &'a [u8; 4],
     chunk_data: &'a [u8],
-    crc: &'a [u8; 4]
+    crc: &'a [u8; 4],
 }
 
-/// Been getting them from here [https://www.w3.org/TR/PNG-Chunks.html#:~:text=A%20valid%20PNG%20image%20must,chunks%2C%20and%20an%20IEND%20chunk.] 
+/// Been getting them from here [https://www.w3.org/TR/PNG-Chunks.html#:~:text=A%20valid%20PNG%20image%20must,chunks%2C%20and%20an%20IEND%20chunk.]
 /// this is all the structs for each chunk type possible
 struct IHDR {}
 struct IDAT {}
@@ -43,5 +84,3 @@ struct zTXT {}
 
 // I think these can be ignored so maybe no point ?
 struct AnicillaryChunk {}
-
-
